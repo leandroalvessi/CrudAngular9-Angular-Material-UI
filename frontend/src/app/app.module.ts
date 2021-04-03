d80b10fe3f4d157256d3ac21004861d24b6e983b
Leandro <leandroalves.desenvolvedor@gmail.com>
2021-04-03 13:57:25 -0300

    #13 - Atualizar produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -32,7 +32,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 
 import localePt from '@angular/common/locales/pt';
-import { registerLocaleData } from '@angular/common'
+import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
 registerLocaleData(localePt);
 
 @NgModule({
@@ -46,7 +47,8 @@ registerLocaleData(localePt);
     RedDirective,
     ForDirective,
     ProductCreateComponent,
-    ProductReadComponent
+    ProductReadComponent,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
